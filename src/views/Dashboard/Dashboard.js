c597edce269e21c1d9e5139f74914abfc161ead9
Matthew Moya <matthewmoya@Matthews-MacBook-Pro.local>
2019-05-15 17:21:14 -0700

    thats it for today

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -5,27 +5,8 @@ import Ad from '../Ad';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.toggle = this.toggle.bind(this);
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-    this.state = {
-      dropdownOpen: false,
-      radioSelected: 2,
-    };
+    this.state = {};
   }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
-
-  onRadioBtnClick(radioSelected) {
-    this.setState({
-      radioSelected: radioSelected,
-    });
-  }
-
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
   render() {
     return (
